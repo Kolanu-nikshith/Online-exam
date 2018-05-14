98d82a7f23c022a46f0232d5eb84e1b757820f80
kolanu-nikshith <nkreddy100@gmail.com>
2018-05-14 14:34:36 +0530

    alter delete removed

--- admin/navbar.php
+++ admin/navbar.php
@@ -21,34 +21,12 @@
 							<li><a href="addsub.php">Subject</a></li>
 							<li><a href="addtest.php">Test</a></li>
 						</ul></li>
-					<li><a class="dropdown-toggle" data-toggle="dropdown" href="#">Alter<span
-							class="caret"></span></a>
-						<ul class="dropdown-menu" role="menu">
-							<li><a href="">Student</a></li>
-							<li><a href="">Faculty</a></li>
-							<li><a href="">Subject</a></li>
-							<li><a href="">Test</a></li>
-						</ul></li>
+					
 
-					<li><a class="dropdown-toggle" data-toggle="dropdown" href="#">Delete<span
-							class="caret"></span></a>
-						<ul class="dropdown-menu" role="menu">
-							<li><a href="delstudent.php">Student</a></li>
-							<li><a href="delfaculty.php">Faculty</a></li>
-							<li><a href="delsubject.php">Subject</a></li>
-							<li><a href="deltest.php">Test</a></li>
-							<!--<li><a href="sub/departments/s&h/civilv&m.php">S & H</a></li>-->
-						</ul></li>
-						<li><a class="dropdown-toggle" data-toggle="dropdown" href="#">View<span
-							class="caret"></span></a>
-						<ul class="dropdown-menu" role="menu">
-							<li><a href="">Student</a></li>
-							<li><a href="">Faculty</a></li>
-							<li><a href="">Subject</a></li>
-							<li><a href="">Test</a></li>
-							<!--<li><a href="sub/departments/s&h/civilv&m.php">S & H</a></li>-->
-						</ul></li>
-
+					
+						
+					<li><a href="delfaculty.php">Faculty</li>
+					<li><a href="delstudent.php">Students</li>
 					<li><a href="liveexams.php">Live Exams</li>
 
 					<li><a href="generate.php">Generate Report</li>
@@ -95,34 +73,11 @@
 							<li><a href="addtest.php">Test</a></li>
 						</ul>
 					</li>
-					<li><a class="dropdown-toggle" data-toggle="dropdown" href="#">Alter<span class="caret"></span></a>
-						<ul class="dropdown-menu" role="menu">
-							<li><a href="">Student</a></li>
-							<li><a href="">Faculty</a></li>
-							<li><a href="">Subject</a></li>
-							<li><a href="">Test</a></li>
-						</ul>
-					</li>
+					
 
-					<li><a class="dropdown-toggle" data-toggle="dropdown" href="#">Delete<span class="caret"></span></a>
-						<ul class="dropdown-menu" role="menu">
-							<li><a href="delstudent.php">Student</a></li>
-							<li><a href="delfaculty.php">Faculty</a></li>
-							<li><a href="delsubject.php">Subject</a></li>
-							<li><a href="deltest.php">Test</a></li>
-							<!--<li><a href="sub/departments/s&h/civil.php">S & H</a></li>-->
-						</ul>
-					</li>
-
-					<li><a class="dropdown-toggle" data-toggle="dropdown" href="#">View<span class="caret"></span></a>
-						<ul class="dropdown-menu" role="menu">
-							<li><a href="">Student</a></li>
-							<li><a href="">Faculty</a></li>
-							<li><a href="">Subject</a></li>
-							<li><a href="">Test</a></li>
-						</ul>
-					</li>
-
+					
+					<li><a href="delfaculty.php">Faculty</a></li>
+					<li><a href="delstudent.php">Students</a></li>
 					<li><a href="liveexams.php">Live Exams</a></li>
 					<li><a href="generate.php">Generate Report</a></li>
 					<li><a href="">Contact Us</a></li>
